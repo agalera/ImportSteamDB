b7a788840e5f419031c69111fe45fb1c2a3d2292
alberto_galera <galerajimenez@gmail.com>
2014-09-19 10:24:52 +0200

    pep8

--- generate_db.py
+++ generate_db.py
@@ -6,18 +6,22 @@ from pymongo import MongoClient
 c = MongoClient()
 db = c.Steam
 db.games.drop()
+api_url = 'http://store.steampowered.com/api/'
+
+
 def get_json(url):
     try:
         return json.loads(requests.get(url).content)
     except:
         return None
 
-lista = get_json('http://api.steampowered.com/ISteamApps/GetAppList/v2?format=json')
-pool_sema = BoundedSemaphore(100)
+
 def consigue(game):
     try:
-        result = get_json('http://store.steampowered.com/api/appdetails/?appids='+str(game['appid']))[str(game['appid'])]['data']
+        url = 'http://store.steampowered.com/api/appdetails/?appids='
+        result = get_json('{}{}'.format(url, game['appid']))
         #platforms
+        #voluntary no individual except
         platforms = []
         for x in result['platforms']:
             if result['platforms'][x]:
@@ -30,12 +34,12 @@ def consigue(game):
             for x in result['categories']:
                 categories.append(x['description'])
         except:
-            print "falla categories"
+            print "ko categories"
         try:
             for x in result['genres']:
                 categories.append(x['description'])
         except:
-            print "falla genres"
+            print "ko genres"
         result['categories'] = categories
 
         #supported_languages
@@ -44,16 +48,19 @@ def consigue(game):
             for x in result['supported_languages'].split():
                 supported_languages.append(x.strip())
         except:
-            print "falla supported_languages"
+            print "ko supported_languages"
         result['supported_languages'] = supported_languages
         db.games.insert(result)
         print "ok", game['appid']
     except:
-        print "fallo", game['appid']
+        print "ko", game['appid']
     finally:
         pool_sema.release()
 
+url = 'http://api.steampowered.com/ISteamApps/GetAppList/v2?format=json'
+lista = get_json(url)
+pool_sema = BoundedSemaphore(100)
 for game in lista["applist"]["apps"]:
     pool_sema.acquire()
-    thread = Thread(target = consigue, args = (game, ))
+    thread = Thread(target=consigue, args=(game, ))
     thread.start()
